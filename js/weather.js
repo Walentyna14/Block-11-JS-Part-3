ae8310adbbe7354cb7fb374eafb32fdc09ad8347
Kamila Kosmowska <kamilawkosmowska@gmail.com>
2017-04-04 19:43:48 +0200

    Add files via upload

--- js/weather.js
+++ js/weather.js
@@ -1,36 +1,41 @@
-if ("geolocation" in navigator){
-    navigator.geolocation.getCurrentPosition(function(position){
-        loadWeather(position.coords.latitude +','+ position.coords.longitude);
-    });
-} else{
-    loadWeather("Warsaw, PL", "");
+function getWeather() {
+	function success(pos) {
+		var crd = pos.coords;
+		loadWeather(crd.latitude +','+crd.longitude);
+	};
+
+	function error() {
+		//Warszawa
+		loadWeather(52.2048418 +','+ 20.911452);
+	};
+
+	navigator.geolocation.getCurrentPosition(success, error);
 
 }
-    
 $(document).ready(function(){
-    setInterval(getWeather, 10000);
+	setInterval(getWeather(), 10000);
 });
 
-function loadWeather(location, woeid){
-    $.simpleWeather({
-        location: location,
-        woeid: woeid,
-        unit: 'c',
-        success: function(weather){
-            city = weather.city;
-            temp = weather.temp+'&deg;';
-            wcode = '<img class="weathericon" src="images/weathericons/' + weather.code +'.svg">';
-            wind = '<p>'+ weather.wind.speed +'</p><p>'+ weather.units.speed + '</p>';
-            humidity = weather.humidity +'%';
-            
-            $('.location').text(city);
-            $('.temperature').html(temp);
-            $('.climate_bg').html(wcode);
-            $('.windspeed').html(wind);
-            $('.humidity').text(humidity);
-        },
-        error: function(error){
-            $('.error').html ('<p>'+error+'</p>');
-        }
-    });   
+function loadWeather(location, woeid) {
+	$.simpleWeather({
+		location: location,
+		woeid: woeid,
+		unit: 'c',
+		success: function(weather) {
+			city = weather.city;
+			temp = weather.temp+'&deg;';
+			wcode = '<img class="weathericon" src="images/weathericons/' + weather.code + '.svg">';
+			wind = '<p>' + weather.wind.speed + '</p><p>' + weather.units.speed + '</p>';
+			humidity = weather.humidity + ' %';
+
+			$(".location").text(city);
+			$(".temperature").html(temp);
+			$(".climate_bg").html(wcode);
+			$(".windspeed").html(wind);
+			$(".humidity").text(humidity);
+		},
+		error: function(error) {
+			$(".error").html('<p>' + error + '</p>');
+		}
+	});
 }
